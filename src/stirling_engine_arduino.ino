38307252cf1926847a6a042fa453b450c257e3f7
alxzhng <65534431+alxzhng@users.noreply.github.com>
2023-02-23 15:33:41 +0000

    Fix issue with sensors
    temperature sensor / ir sensor interference

--- src/stirling_engine_arduino.ino
+++ src/stirling_engine_arduino.ino
@@ -4,18 +4,18 @@ UCL MechEng Y1 Stirling Engine Lab (Arduino Part)
 For information on usage, visit https://github.com/alxzhng/stirling_engine_code.
 
  ************************************************/
-#include <SoftwareSerial.h>
 
+//---------------------- SET UP ------------------------//
 // Pin Definitions
-#define buttonPin 3       // pin connected to button (digital input)
-#define irPin 5           // pin connected to IR sensor
-#define ledPin 13           // pin connected to green LED
-#define tempPin1 A0       // pin connected to temp sensor 1 (analogue input)
-#define tempPin2 A1       // temp sensor 2
+#define buttonPin 3          // pin connected to button (digital input)
+#define irSensorPin 5        // pin connected to IR sensor
+#define ledPin 13            // pin connected to green LED
+#define tempPin1 A0          // pin connected to temp sensor 1 (analogue input)
+#define tempPin2 A1          // temp sensor 2
 
 // Options
 unsigned long runTime = 10;  // run time in minutes -- button can be used to interrupt this
-long baudrate = 74880;       // frequency at which data is saved -- https://www.arduino.cc/en/Reference/SoftwareSerialBegin
+long baudrate = 9600;      // frequency at which data is written to serial (symbols per second)
 
 // Initialize input variables
 int irState;              // ir sensor trigger
@@ -23,18 +23,25 @@ int buttonState;          // button to start/stop measurement
 unsigned long startTime, currentTime, delta_t;
 float period, freq;       // time taken for one revolution
 float temp1, temp2;       // temperature readings
+int volt1, volt2;
+
+// Storage variables for printing to serial
+char str[100];
+char temp1str[30];
+char temp2str[30];
+
+//---------------------- MAIN CODE ------------------------//
 
 void setup() {
   runTime = runTime * 60000;   // convert run time to milliseconds
 
   pinMode(ledPin, OUTPUT);
-  pinMode(irPin, INPUT);
+  pinMode(irSensorPin, INPUT);
   pinMode(tempPin1, INPUT);
   pinMode(tempPin2, INPUT);
   pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
 
-  digitalWrite(irPin, LOW);
+  digitalWrite(irSensorPin, HIGH);
   digitalWrite(ledPin, LOW);
 
   Serial.begin(baudrate);
@@ -42,14 +49,12 @@ void setup() {
 }
 
 void loop() {
-  // Do nothing if button not pressed
-  while (true)
+  while (true)                            // Do nothing if button not pressed
   {
     sleep();
     buttonState = digitalRead(buttonPin);
 
-    // Button has been pressed, DO SOMETHING!!
-    if (buttonState == HIGH)
+    if (buttonState == HIGH)              // Button has been pressed, DO SOMETHING!!
     {
       break;
     }
@@ -72,27 +77,23 @@ void loop() {
   delay(500);
   Serial.println("Writing data. Press button again to terminate.");
 
-  startTime = millis(); // take start time 
+  startTime = millis();                 // Take start time 
 
-  digitalWrite(ledPin, HIGH); // set state indication high 
+  digitalWrite(ledPin, HIGH);           // Set state indication high 
 
   while (true)
   {
-    getTemperature(); // take temperature readings
+    // Read input pins
+    volt1 = analogRead(tempPin1);       // Temperature sensor 1 (upper)
+    volt2 = analogRead(tempPin2);       // Temperature sensor 2 (lower)
+    irState = digitalRead(irSensorPin); // IR receiver state -- 0 if broken, 1 if broken
 
-    irState = digitalRead(irPin); // take ir beam break -- 1 if broken, 0 if unbroken
-
-    currentTime = millis(); // update timer
-    delta_t = currentTime - startTime; // time elapsed since measurement start, in milliseconds
+    currentTime = millis();             // Update timer counter
+    delta_t = currentTime - startTime;  // Time elapsed since measurement start, in milliseconds
 
     // Store sensor readings as formatted string
-    char str[100];
-    char temp1str[30];
-    char temp2str[30];
-    dtostrf(temp1, 3, 3, temp1str);
-    dtostrf(temp2, 3, 3, temp2str);
-    sprintf(str, "%6ld,%s,%s,%d", delta_t, temp1str, temp2str, irState);
-
+    sprintf(str, "%6ld,%d,%d,%d", delta_t, volt1, volt2, irState);
+    
     // Print string to serial
     Serial.println(str);
 
@@ -103,7 +104,7 @@ void loop() {
     }
   }
 
-  Serial.println("Done taking measurements.");
+  Serial.println("Done taking measurements."); 
   digitalWrite(ledPin, LOW);
 
   // Go back to doing nothing
@@ -111,23 +112,6 @@ void loop() {
   {
     sleep();
   }
-}
-
-void getTemperature()
-{
-  int volt1 = analogRead(tempPin1);
-  int volt2 = analogRead(tempPin2);
-
-  // Convert ADC to voltage in mV
-  temp1 = (float) volt1; // 5v, 1024 adc
-  temp2 = (float) volt2;
-
-  temp1 = temp1 * 5000.0 / 1024.0;
-  temp2 = temp2 * 5000.0 / 1024.0;
-
-  // Refer to TMP36 datasheet for output characteristics
-  temp1 = (temp1 - 500)/10; // offset voltage 0.5v, scaling 10mV/C
-  temp2 = (temp2 - 500)/10;
 }
 
 void sleep()
